8e380ccab0530154f2ab0954c90f6e8ff7e1dc22
Anar Rzayev <anarrzayev@Anars-MacBook-Pro.local>
2018-11-01 04:08:48 +0400

    inital commit

---
+++ api/src/Controller/ServiceController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Service;
+use App\Form\ServiceType;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Form\FormInterface;
+
+class ServiceController extends AbstractController
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/services", methods={"POST"})
+     */
+    public function services(Request $request, ValidatorInterface $validator)
+    {
+        $service = new Service();
+
+        $service->setCreatedDate(new \DateTime());
+        $service->setUpdatedDate(new \DateTime());
+        $service->setActive(1);
+
+        // decode json data php array
+        $data = json_decode(
+            $request->getContent(), 
+            true
+        );
+        
+        $form = $this->createForm(ServiceType::class, $service);
+    
+        $form->submit($data);
+        
+        if (false === $form->isValid()) {
+            $errors = $this->getErrorMessages($form);
+            
+            return new JsonResponse(
+                [
+                    'status' => false,
+                    'errors' => $errors,
+                ],
+                JsonResponse::HTTP_BAD_REQUEST
+            );
+        }
+
+        // if pass validation then save data
+        $this->entityManager->persist($form->getData());
+        $this->entityManager->flush();
+
+        $savedService = $this->getDoctrine()
+        ->getRepository(Service::class)
+        ->find($service->getId());
+        
+        $data = (object)[
+            'id' => $savedService->getId(),
+            'title' => $savedService->getTitle(),
+            'description' => $savedService->getDescription(),
+            'end_date' => $savedService->getEndDate(),
+        ];
+        
+        return new JsonResponse(
+            [
+                'status' => true,
+                'data' => $data
+            ],
+            JsonResponse::HTTP_CREATED
+        );
+    }
+
+    public function getErrorMessages(FormInterface $form)
+    {
+        $errors = array();
+    
+        //this part get global form errors (like csrf token error)
+        foreach ($form->getErrors() as $error) {
+            $errors[] = $error->getMessage();
+        }
+    
+        //this part get errors for form fields
+        /** @var Form $child */
+        foreach ($form->all() as $child) {
+            if (!$child->isValid()) {
+                $options = $child->getConfig()->getOptions();
+                //there can be more than one field error, that's why implode is here
+                $errors[$options['label'] ? $options['label'] : ucwords($child->getName())] = implode('; ', $this->getErrorMessages($child));
+            }
+        }
+    
+        return $errors;
+    }
+}
---
+++ api/src/Entity/Service.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ServiceRepository")
+ */
+class Service
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $service_id;
+
+    /**
+     * @ORM\Column(type="string", length=120)
+     * @Assert\NotBlank()
+     * @Assert\Length(
+     *      min = 5,
+     *      max = 50,
+     *      minMessage = "Title must be at least {{ limit }} characters long",
+     *      maxMessage = "Title cannot be longer than {{ limit }} characters"
+     * )
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @Assert\NotBlank() 
+     */
+    private $city_id;
+
+    /**
+     * @ORM\Column(type="string")
+     * @Assert\NotBlank() 
+     * @Assert\Length(max=5)
+     */
+    private $zip_code;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $end_date;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_date;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updated_date;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $active;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getServiceId(): ?int
+    {
+        return $this->service_id;
+    }
+
+    public function setServiceId(int $service_id): self
+    {
+        $this->service_id = $service_id;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getCityId(): ?int
+    {
+        return $this->city_id;
+    }
+
+    public function setCityId(int $city_id): self
+    {
+        $this->city_id = $city_id;
+
+        return $this;
+    }
+
+    public function getZipCode(): ?string
+    {
+        return $this->zip_code;
+    }
+
+    public function setZipCode(?string $zip_code): self
+    {
+        $this->zip_code = $zip_code;
+
+        return $this;
+    }
+
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getEndDate(): ?\DateTimeInterface
+    {
+        return $this->end_date;
+    }
+
+    public function setEndDate(\DateTimeInterface $end_date): self
+    {
+        $this->end_date = $end_date;
+
+        return $this;
+    }
+
+    public function getCreatedDate(): ?\DateTimeInterface
+    {
+        return $this->created_date;
+    }
+
+    public function setCreatedDate(\DateTimeInterface $created_date): self
+    {
+        $this->created_date = $created_date;
+
+        return $this;
+    }
+
+    public function getUpdatedDate(): ?\DateTimeInterface
+    {
+        return $this->updated_date;
+    }
+
+    public function setUpdatedDate(\DateTimeInterface $updated_date): self
+    {
+        $this->updated_date = $updated_date;
+
+        return $this;
+    }
+
+    public function getActive(): ?int
+    {
+        return $this->active;
+    }
+
+    public function setActive(int $active): self
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+}
